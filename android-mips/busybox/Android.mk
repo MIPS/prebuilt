221e0eab5076fa984e7ab48c8cb0c1a6589f6405
Benoit Goby <benoit_goby@sdesigns.com>
2010-01-11 15:21:24 -0800

    Added busybox (statically linked)

---
+++ android-mips/busybox/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# Explicitly mark busybox as "eng" so that it doesn't
+# get included in user or SDK builds. (GPL issues)
+#
+LOCAL_SRC_FILES := busybox 
+LOCAL_MODULE := busybox
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := debug
+include $(BUILD_PREBUILT)
+
+# Any prebuilt files with default TAGS can use the below:
+prebuilt_files :=
+
+$(call add-prebuilt-files, EXECUTABLES, $(prebuilt_files))
+
