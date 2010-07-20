56f2d405e0d17554ea1b5a413a078914d6930f30
Chris Dearman <chris@mips.com>
2010-07-20 16:02:03 -0700

    [MIPS] Prebuilt kernel & gdbserver
    Signed-off-by: Chris Dearman <chris@mips.com>

---
+++ android-mips/gdbserver/Android.mk
@@ -0,0 +1,18 @@
+ifneq ($(ARCH_HAS_BIGENDIAN),true)
+    LOCAL_PATH := $(call my-dir)
+    include $(CLEAR_VARS)
+
+# Explicitly mark gdbserver as "eng" so that it doesn't
+# get included in user or SDK builds. (GPL issues)
+#
+    LOCAL_SRC_FILES := gdbserver
+    LOCAL_MODULE := gdbserver
+    LOCAL_MODULE_CLASS := EXECUTABLES
+    LOCAL_MODULE_TAGS := debug
+    include $(BUILD_PREBUILT)
+
+# Any prebuilt files with default TAGS can use the below:
+    prebuilt_files :=
+
+    $(call add-prebuilt-files, EXECUTABLES, $(prebuilt_files))
+endif
